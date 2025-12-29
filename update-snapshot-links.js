f496b7f26f1d28bdf6bffe4487dd0b976225351c
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-12-29 23:09:51 +0000

    test: update github actions snapshot

--- update-snapshot-links.js
+++ update-snapshot-links.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const semver = require('semver');
 const { version } = require('jest/package.json');
 
+// Jest v30 changed the snapshot header link
 const headerLink = semver.satisfies(version, '30')
   ? 'https://jestjs.io/docs/snapshot-testing'
   : 'https://goo.gl/fbAQLP';
@@ -18,3 +19,19 @@ for (const file of fs.readdirSync('./integrationTests/__snapshots__')) {
     'utf-8',
   );
 }
+
+// Jest v30.1.0 apparently removed a newline from its github actions reporter
+const ghaSnapshotContent = fs.readFileSync(
+  './integrationTests/__snapshots__/github-actions.test.js.snap',
+  'utf-8',
+);
+
+const extraContent = semver.lt(version, '30.1.0') ? '%0A' : '';
+
+fs.writeFileSync(
+  './integrationTests/__snapshots__/github-actions.test.js.snap',
+  ghaSnapshotContent
+    .replace(/is not defined.%0A%0A/g, 'is not defined.%0A')
+    .replace(/is not defined.%0A/g, 'is not defined.%0A' + extraContent),
+  'utf-8',
+);
